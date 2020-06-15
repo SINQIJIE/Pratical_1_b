a46bc49105e8282338bd2f07fd5bf74fa36fdf18
SINQIJIE <sinqj-wm19@student.tarc.edu.my>
2020-06-15 17:53:41 +0800

    commit buy handsomeboy

---
+++ app/src/main/java/com/example/pratical_1_b/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pratical_1_b;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
